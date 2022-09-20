099ee79dac19086805ae1aa3b04803e942f411bd
Ahmed Osama <ahmed.osama8282@gmail.com>
2022-09-20 16:11:51 +0200

    Create UART.c

---
+++ UART.c
@@ -0,0 +1,19 @@
+#include "UART.h"
+
+void SWUART_init(uint32_t baudrate)
+ {
+    UCSRB |= (1 << RXEN) | (1 << TXEN);
+	  UCSRC |= (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1) | (1 << UPM1);
+	  UBRRL = BAUD_PRESCALE;
+	  UBRRH = (BAUD_PRESCALE >> 8);
+ }
+ void SWUART_send(uint8_t data)
+ {
+    while (! (UCSRA & (1<<UDRE)));
+	  UDR = ch;
+ }
+ void SWUART_recieve(uint8_t *data)
+ {
+    while ((UCSRA & (1 << RXC)) == 0);
+	  return(UDR);
+ }
