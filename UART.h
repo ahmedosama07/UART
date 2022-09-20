55de778ccfd7e7d7cdd8940ea79fd1917f995db8
Ahmed Osama <ahmed.osama8282@gmail.com>
2022-09-20 16:07:14 +0200

    Create UART.h

---
+++ UART.h
@@ -0,0 +1,10 @@
+############# fileName.h ##############
+#ifndef UART_H_
+#define UART_H_
+
+void SWUART_init(uint32_t baudrate);
+void SWUART_send(uint8_t data);
+void SWUART_recieve(uint8_t *data);
+
+#endif
+//////////////////////////////////////////////////////////
